769b9f12145e2a8759ae288249aca03e9f834e83
seunghyecho <tmdgp523@naver.com>
2021-01-11 22:18:50 +0900

    검색버튼 클릭 시, 유투브 api 실행

--- js/main/intro/intro.js
+++ js/main/intro/intro.js
@@ -1,57 +1,57 @@
 $(function(){
     console.log('main intro');
 
-    //load_youtube
-    var URL = 'https://www.googleapis.com/youtube/v3/playlistItems';
-    var key = 'AIzaSyCJRbPH-vDO1Ryn3iZERfc5QQb9nD1sv08';
-    var playlistId = 'PLNjtvANfBJzdwUB22u2u4KzZdZNazkq_y';
+//     //load_youtube
+//     var URL = 'https://www.googleapis.com/youtube/v3/playlistItems';
+//     var key = 'AIzaSyCJRbPH-vDO1Ryn3iZERfc5QQb9nD1sv08';
+//     var playlistId = 'PLNjtvANfBJzdwUB22u2u4KzZdZNazkq_y';
 
-    var options = {
-        part: 'snippet',
-        key: key,
-        maxResults: 10,
-        playlistId: playlistId
-    }
+//     var options = {
+//         part: 'snippet',
+//         key: key,
+//         maxResults: 10,
+//         playlistId: playlistId
+//     }
 
-    load_youtube();
+//     load_youtube();
 
-    //youtube 데이터 호출
-    function load_youtube(){
-        $.ajax({
-            url : URL,
-            dataType : 'jsonp',
-            data : options      
-        })
-        .success(function(data){
-            console.log(data);
-            create_list(data);
-        })
-        .error(function(){
-            alert('Fail to load Youtube data!!');
-        })
-    }
+//     //youtube 데이터 호출
+//     function load_youtube(){
+//         $.ajax({
+//             url : URL,
+//             dataType : 'jsonp',
+//             data : options      
+//         })
+//         .success(function(data){
+//             console.log(data);
+//             create_list(data);
+//         })
+//         .error(function(){
+//             alert('Fail to load Youtube data!!');
+//         })
+//     }
 
-    //유튜브 목록 생성
-function create_list(data){
-    $(data.items).each(function(index, item){
-        var thumb = item.snippet.thumbnails.medium.url;
-        var title = item.snippet.title;
-        var details = item.snippet.description;
-        var details_len = details.length;
-        var vid_id = item.snippet.resourceId.videoId;
-        var date = item.snippet.publishedAt.substring(0,10);
-        var result = '';
+//     //유튜브 목록 생성
+// function create_list(data){
+//     $(data.items).each(function(index, item){
+//         var thumb = item.snippet.thumbnails.medium.url;
+//         var title = item.snippet.title;
+//         var details = item.snippet.description;
+//         var details_len = details.length;
+//         var vid_id = item.snippet.resourceId.videoId;
+//         var date = item.snippet.publishedAt.substring(0,10);
+//         var result = '';
        
 
-        if(details_len>50){
-            result = details.substring(0,50)+'...';
-        }else{
-            result = details;
-        }
+//         if(details_len>50){
+//             result = details.substring(0,50)+'...';
+//         }else{
+//             result = details;
+//         }
 
    
-    })
-}
+//     })
+// }
 
     // metadata : s
     var $intro = $('#intro');
--- src/main/header/header.js
+++ src/main/header/header.js
@@ -6,8 +6,82 @@ $(function(){
     var $icons = $inner.children('.icons');
     var $menuBtn = $('.menuBtn');
     var $backBtn = $('.backBtn');
+    var $searchBtn = $('.searchBtn');
     var $searchLayout = $('#searchLayout');
     var $settingLayout = $('#settingLayout');
+    var $upNext = $('.upNext');
+
+    //load_youtube
+    var URL = 'https://www.googleapis.com/youtube/v3/playlistItems';
+    var key = 'AIzaSyCJRbPH-vDO1Ryn3iZERfc5QQb9nD1sv08';
+    var playlistId = 'PLNjtvANfBJzdwUB22u2u4KzZdZNazkq_y';
+
+    var options = {
+        part: 'snippet',
+        key: key,
+        maxResults: 10,
+        playlistId: playlistId
+    }
+
+    //youtube 데이터 호출
+    function load_youtube(){
+        $.ajax({
+            url : URL,
+            dataType : 'jsonp',
+            data : options      
+        })
+        .success(function(data){
+            console.log(data);
+            create_list(data);
+        })
+        .error(function(){
+            alert('Fail to load Youtube data!!');
+        })
+    }
+    
+     //유튜브 목록 생성
+    function create_list(data){
+
+        $(data.items).each(function(index, item){
+            var thumb = item.snippet.thumbnails.medium.url;
+            var title = item.snippet.title;
+            var channelId = item.snippet.channelId;
+            var vid_id = item.snippet.resourceId.videoId;
+            var date = item.snippet.publishedAt.substring(0,10);
+            var result = '';
+
+            $(".upNext ul.items").append(
+                $('<li class="item">').append(
+                    $('<a href="#">').append(
+                            $('<div class="video">').append("<img src='" + thumb + "'/>"),
+                            $('<div class="info">').append(
+                                $('<div class="avatar">').append('<img src="img/avatar.jpg" alt="avatar">'),
+                                $('<div class="titleBtn">').append(
+                                    $('<div class="title">').text(title),
+                                    $('<ul>').append(
+                                        $('<li class="name">').text(channelId),
+                                        $('<li class="view">').text(vid_id),
+                                        $('<li class="late">').text(date),
+                                    ),
+                                    $('<button class="moreBtn">').append(
+                                        $('<i class="fas fa-ellipsis-v" aria-hidden="true">')
+                                    )
+                                )
+                            )
+                    )
+                )
+            );                
+            
+            
+       
+        })
+    }
+
+    $searchBtn.on('click', function(){
+        $searchLayout.hide();
+        load_youtube();
+        $upNext.addClass('on');
+    });
 
     //공통 : 외부영역 클릭 시 팝업창 숨김
     $(document).mouseup(function(e){
